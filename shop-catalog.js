bbfd278653651466573dc0659255bd534fbe7fab
EugeneRuzanov <eugene.ruzanov@gmail.com>
2016-08-21 17:12:23 +0300

    -

--- shop-catalog.js
+++ shop-catalog.js
@@ -17,31 +17,66 @@ window.addEventListener( 'scroll', function () {
     sticky.classList[ window.pageYOffset >= 136 ? 'add' : 'remove' ]( 'sticky' );
 } );
 
-// insert tree of sub menu
+//<div class="sub-menu_content">
+//  <div class="sub-menu_content_item">
+//    <div class="sub-menu_content_item-title">Аксессуары для ванной</div>
+//    <ul>
+//      <li>Аксессуары</li>
+//      <li>Занавес</li>
+//      <li>Зеркала</li>
+//      <li>Карнизы</li>
+//      <li>Коврики</li>
+//      <li>Полотенцедержатели</li>
+//    </ul>
+//  </div>
+//</div>
 function generateSubMenuContent ( list ) {
-    var wrapper = document.createElement( 'div' ),
+    var subMenuContent = document.createElement( 'div' ),
+        subMenuContentItem = document.createElement( 'div' ),
+        subMenuContentItemTitle = document.createElement( 'div' ),
         ul = document.createElement( 'ul' ),
-        li;
-    wrapper.classList.add( 'sub-menu_content' );
-    wrapper.appendChild( ul );
-    for ( var i = 0; i < list.length; i++ ) {
         li = document.createElement( 'li' );
-        li.textContent = list[i].name;
-        ul.appendChild( li );
+    subMenuContent.classList.add( 'sub-menu_content' );
+    subMenuContentItem.classList.add( 'sub-menu_content_item' );
+    subMenuContentItemTitle.classList.add( 'sub-menu_content_item-title' );
+
+    for ( var i = 0; i < list.length; i++ ) {
+        subMenuContentItem.innerHTML = '';
+        subMenuContentItemTitle.textContent = list[i].name;
+        subMenuContentItem.appendChild( subMenuContentItemTitle.cloneNode( true ) );
+        if ( list[i].sub ) {
+            ul.innerHTML = '';
+            console.log( list[i].sub.length );
+            for ( var j = 0; j < list[i].sub.length; j++ ) {
+                li.textContent = list[i].sub[j].name;
+                ul.appendChild( li.cloneNode( true ) );
+            }
+            subMenuContentItem.appendChild( ul.cloneNode( true ) );
+        }
+        subMenuContent.appendChild( subMenuContentItem.cloneNode( true ) );
     }
-    return wrapper;
+
+    return subMenuContent;
 }
+//<li class="sub-menu_item">
+//  <a href="#">Керамогранит</a>
+//</li>
 function generateSubMenu ( list ) {
     var frag = document.createDocumentFragment(),
+        li = document.createElement( 'li' ),
         a = document.createElement( 'a' );
     a.href = '#';
-    a.classList.add( 'sub-menu_item' );
-    for ( var i = 0, item; i < list.length; i++ ) {
-        item = a.cloneNode();
-        item.textContent = list[i].name;
-        frag.appendChild( item );
+    li.classList.add( 'sub-menu_item' );
+    for ( var i = 0, node, nodeLink; i < list.length; i++ ) {
+        node = li.cloneNode();
+        nodeLink = a.cloneNode();
+        nodeLink.textContent = list[i].name;
+        node.appendChild( nodeLink );
+        if ( list[i].sub ) {
+            node.appendChild( generateSubMenuContent( list[i].sub ) );
+        }
+        frag.appendChild( node );
     }
-    console.log( frag );
     return frag;
 }
 document.querySelector( '.sub-menu_list').innerHTML = '';
@@ -50,18 +85,32 @@ document.querySelector( '.sub-menu_list' ).appendChild( generateSubMenu( shopCat
 // set dimension for sub-menu
 var setDimension = ( function () {
     var subMenu = document.querySelector( '.sub-menu_list' ),
-        subMenuContent = document.querySelector( '.sub-menu_content' );
+        subMenuContent = document.querySelectorAll( '.sub-menu_content' );
     return function () {
         var windowHeight = document.documentElement.clientHeight,
             mainMenu = document.querySelector( '.main-menu' ),
             offsetTop = mainMenu.getBoundingClientRect().top < 0 ? 0 : mainMenu.getBoundingClientRect().top,
             height = windowHeight - mainMenu.offsetHeight - offsetTop - 30;
-        subMenuContent.style.height = subMenu.style.height = height + 'px';
-        //subMenuContent.forEach( function ( el ) {
-        //    el.style.height = height + 'px';
-        //} );
+        subMenu.style.height = height + 'px';
+        subMenuContent.forEach( function ( el ) { el.style.height = height + 'px'; } );
     };
 } )();
 window.addEventListener( 'scroll', setDimension );
 window.addEventListener( 'resize', setDimension );
 setDimension();
+
+// toggle visible sub menu contents
+var subMenu = document.querySelector( '.sub-menu' );
+var subMenuList = document.querySelector( '.sub-menu_list' );
+subMenuList.addEventListener( 'mouseover', function ( e ) {
+    var subMenuItem = e.target.closest( '.sub-menu_item' );
+    if ( subMenuItem.querySelector( '.sub-menu_content' ) ) {
+        subMenu.style.width = '630px';
+    }
+} );
+subMenuList.addEventListener( 'mouseout', function ( e ) {
+    var subMenuItem = e.target.closest( '.sub-menu_item' );
+    if ( subMenuItem.querySelector( '.sub-menu_content' ) ) {
+        subMenu.style.width = '315px';
+    }
+} );
